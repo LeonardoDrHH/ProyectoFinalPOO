94a2cd4bf8ce5b077b244b3d0539589bd78884d2
Lonarobh06 <lrobleshinojosa230@gmail.com>
2025-05-19 10:33:35 -0700

    FrameFactura

--- ProyectoFinalPOO/src/backEnd/Patentes/FacturaPatente.java
+++ ProyectoFinalPOO/src/backEnd/Patentes/FacturaPatente.java
@@ -9,5 +9,81 @@ package backEnd.Patentes;
  * @author lrobl
  */
 public class FacturaPatente {
+    private static int contador = 1;
+    private int numeroFactura;
+    private String horaFactura;
+    private String nombreSolicitante;
+    private String nombreProducto;
+    private String descripcionTecnica;
+
+    public FacturaPatente(int numeroFactura, String horaFactura, String nombreSolicitante, String nombreProducto, String descripcionTecnica) {
+        this.numeroFactura = contador++;
+        this.horaFactura = horaFactura;
+        this.nombreSolicitante = nombreSolicitante;
+        this.nombreProducto = nombreProducto;
+        this.descripcionTecnica = descripcionTecnica;
+    }
+
+    
+    public String getHoraFactura() {
+        return horaFactura;
+    }
+
+    public void setHoraFactura(String horaFactura) {
+        this.horaFactura = horaFactura;
+    }
+
+    
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        FacturaPatente.contador = contador;
+    }
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getNombreSolicitante() {
+        return nombreSolicitante;
+    }
+
+    public void setNombreSolicitante(String nombreSolicitante) {
+        this.nombreSolicitante = nombreSolicitante;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getDescripcionTecnica() {
+        return descripcionTecnica;
+    }
+
+    public void setDescripcionTecnica(String descripcionTecnica) {
+        this.descripcionTecnica = descripcionTecnica;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaPatente{" +
+                "\n Número de factura: " + numeroFactura +
+                "\n Hora de factura: " + horaFactura +
+                "\n Nombre solicitante: " + nombreSolicitante +
+                "\n Nombre producto: " + nombreProducto +
+                "\n Descripcion tecnica: " + descripcionTecnica +
+                "\n" + '}';
+    }
+    
     
 }
